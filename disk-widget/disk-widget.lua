ed90ae71e616eaff798a7ee98c18ae39ddd47d26
Miroslav Sumega <miro.sumega@gmail.com>
2023-02-19 20:55:25 +0100

    Fix margins, text aligment and rounding of values

--- disk-widget/disk-widget.lua
+++ disk-widget/disk-widget.lua
@@ -5,41 +5,55 @@ local gears = require("gears")
 local beautiful = require("beautiful")
 
 local disk_widget = {}
-local main_filesystem = '/dev/sdb3'
 
-local function round(number)
-    return math.floor(10 * number / 1024^2 + 0.5) / 10
+local function round_to_gib(number)
+    return math.floor(10 * number / 1024 + 0.5) / 10
 end
 
 local function create_textbox(args)
-    return wibox.widget{
+    local text_widget = wibox.widget{
         text = args.text,
         align = args.align or 'left',
         markup = args.markup,
-        forced_width = args.forced_width or 40,
-        widget = wibox.widget.textbox
+        forced_width = args.forced_width or 10,
+        widget = wibox.widget.textbox,
+    }
+    return wibox.widget{
+        {
+            text_widget,
+            left = args.left_margin or 10,
+            widget = wibox.container.margin
+        },
+        widget = wibox.container.background
     }
 end
 
 local function create_disk_header(params)
   local res = wibox.widget{
-      create_textbox{markup = '<b>Filesystem</b>'},
-      create_textbox{markup = '<b>Size</b>'},
-      create_textbox{markup = '<b>Used</b>'},
-      create_textbox{markup = '<b>Avail</b>'},
-      create_textbox{markup = '<b>Mount</b>'},
+      create_textbox{markup = '<b>Filesystem</b>', align = 'left', left_margin = 0},
+      create_textbox{markup = '<b>Size (GiB)</b>', align = 'right', left_margin = 0},
+      create_textbox{markup = '<b>Used (GiB)</b>', align = 'right'},
+      create_textbox{markup = '<b>Avail (GiB)</b>', align = 'right'},
+      create_textbox{markup = '<b>Mount</b>', align = 'left', left_margin = 20},
       layout = wibox.layout.ratio.horizontal
   }
-  res:ajust_ratio(1, 0, 0.2, 0.8)
-  res:ajust_ratio(2, 0.2, 0.1, 0.7)
-  res:ajust_ratio(3, 0.3, 0.1, 0.6)
-  res:ajust_ratio(4, 0.4, 0.1, 0.5)
+  res:set_ratio(5, 0.4)
 
   return res
 end
 
 local function worker(args)
   local args = args or {}
+
+  local width = 500
+  if args.width ~= nil then
+      width = args.width
+  end
+
+  local main_filesystem = '/dev/sdb3'
+  if args.main_filesystem ~= nil then
+      main_filesystem = args.main_filesystem
+  end
 
   --- Disk widget
   disk_widget = wibox.widget.textbox()
@@ -50,10 +64,10 @@ local function worker(args)
       shape = gears.shape.rect,
       border_width = 1,
       border_color = beautiful.bg_normal,
-      maximum_width = 1000,
+      maximum_width = width,
       offset = { y = 5 },
       widget = {},
-      minimum_width = 500
+      minimum_width = width
   }
 
   local filesystem, size, used, avail, avail_percent, mount
@@ -72,7 +86,7 @@ local function worker(args)
   end)
 
   watch(
-    'bash -c "df -h | tail -n+2"',
+    'bash -c "df -h -BM | tail -n+2"',
     1,
   	function(widget, stdout, stderr, exitreason, exitcode)
       disk_rows = {
@@ -85,11 +99,11 @@ local function worker(args)
           if j == 0 then
             filesystem = w
           elseif j == 1 then
-            size = w:sub(1, -2)
+            size = round_to_gib(w:sub(1, -2))
           elseif j == 2 then
-            used = w:sub(1, -2)
+            used = round_to_gib(w:sub(1, -2))
           elseif j == 3 then
-            avail = w:sub(1, -2)
+            avail = round_to_gib(w:sub(1, -2))
           elseif j == 5 then
             mount = w
           end
@@ -101,17 +115,14 @@ local function worker(args)
         end
 
         row_widget = wibox.widget{
-          create_textbox{text = filesystem},
-          create_textbox{text = size},
-          create_textbox{text = used},
-          create_textbox{text = avail},
-          create_textbox{text = mount},
+          create_textbox{text = filesystem, align = 'left', left_margin = 0},
+          create_textbox{text = size, align = 'right', left_margin = 0},
+          create_textbox{text = used, align = 'right'},
+          create_textbox{text = avail, align = 'right'},
+          create_textbox{text = mount, align = 'left', left_margin = 20},
           layout = wibox.layout.ratio.horizontal
         }
-        row_widget:ajust_ratio(1, 0, 0.2, 0.8)
-        row_widget:ajust_ratio(2, 0.2, 0.1, 0.7)
-        row_widget:ajust_ratio(3, 0.3, 0.1, 0.6)
-        row_widget:ajust_ratio(4, 0.4, 0.1, 0.5)
+        row_widget:set_ratio(5, 0.4)
 
         disk_rows[i] = wibox.widget {
           {
